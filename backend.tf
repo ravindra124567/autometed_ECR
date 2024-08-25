b48a580148680a353acea241c1bda7bcef324949
Manas Roul <manas.scs@gmail.com>
2024-08-26 00:32:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = var.s3_bucket_name
-    key            = var.state_file_key
-    region         = var.aws_region
-    dynamodb_table = var.dynamodb_table_name
+    bucket         = "my-terraform-state-bucket"
+    key            = "terraform/state.tfstate"
+    region         = "us-east-1" 
+    dynamodb_table = "terraform-locks"
   }
 }
