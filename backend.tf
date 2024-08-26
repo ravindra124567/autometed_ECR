174fd6fb80d68f8e457a39520969fdc09d3eabaa
ravindra124567 <129646723+ravindra124567@users.noreply.github.com>
2024-08-26 14:13:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "manas-ecr-terraform-state-bucket128"
+    bucket         = "ravindra-ecr-terraform-state-bucket128"
     key            = "terraform/state.tfstate"
     region         = "us-east-1" 
     dynamodb_table = "ecr-terraform-locks-753157"
